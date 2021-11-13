24dab7cff9beac0db408bfe582955722229cbd6d
Terry Duchastel <terry.duchastel@citrix.com>
2021-11-12 19:54:04 -0500

    Updated to use version 2.4 of Jersey, same as CEM service.

--- src/main/java/com/terrydu/asyncservice/ServletInitializer.java
+++ src/main/java/com/terrydu/asyncservice/ServletInitializer.java
@@ -2,7 +2,10 @@ package com.terrydu.asyncservice;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
